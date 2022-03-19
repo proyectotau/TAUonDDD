b4435065adc53a20ec3493cb0376a01fda38a50f
Javier Garrido <ProyectoTAU2@gmail.com>
2022-03-19 16:55:47 +0100

    First test with Tactician CommandBus

--- tests/Integration/CommandBus/CommandBusTest.php
+++ tests/Integration/CommandBus/CommandBusTest.php
@@ -2,11 +2,81 @@
 
 namespace ProyectoTAU\Tests\Integration\CommandBus;
 
+use League\Tactician\Middleware;
 use PHPUnit\Framework\TestCase;
-use ProyectoTAU\CommandBus\CommandBus;
+use ProyectoTAU\CommandBus\CommandBus as LocalCommandBus;
+use League\Tactician\CommandBus;
+
+class TestLogger
+{
+    private $testcase;
+
+    public function __construct(object $that)
+    {
+        $this->testcase = $that;
+    }
+
+    public function __call($name, $message)
+    {
+        $this->testcase->assertTrue(TRUE);
+        printf("\n[%s] %s\n", $name, $message[0]);
+    }
+}
+
+class LoggerMiddleware implements Middleware
+{
+    private $logger;
+
+    public function __construct(object $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function execute(object $command, callable $next)
+    {
+        $this->logger->info(\get_class($command));
+        $next($command);
+    }
+}
+
+class CommandsRunner implements Middleware
+{
+    public function execute(object $command, callable $next)
+    {
+        $commandName = \get_class($command);
+        $commandHandler = $commandName.'Handler';
+        (new $commandHandler)->handle($command);
+    }
+}
+
+class LocalCommand
+{
+
+}
+
+class LocalCommandHandler
+{
+    public function handle(object $command)
+    {
+        echo PHP_EOL.\get_class($command).' handled'.PHP_EOL;
+    }
+}
 
 class CommandBusTest extends TestCase
 {
+    /**
+     * @link https://github.com/sebastianbergmann/phpunit-documentation/issues/171#issuecomment-67239415
+     */
+    function test_can_make_LocalCommandBus_class(){
+        try {
+            $cmdbus = new LocalCommandBus();
+        } catch (\InvalidArgumentException $notExpected) {
+            $this->fail();
+        }
+
+        $this->assertTrue(TRUE);
+    }
+
     /**
      * @link https://github.com/sebastianbergmann/phpunit-documentation/issues/171#issuecomment-67239415
      */
@@ -16,6 +86,17 @@ class CommandBusTest extends TestCase
         } catch (\InvalidArgumentException $notExpected) {
             $this->fail();
         }
+
+        $this->assertTrue(TRUE);
+    }
+
+    function test_can_make_Middleware_class(){
+        $cmdbus = new CommandBus(
+            new LoggerMiddleware(new TestLogger($this)),
+            new CommandsRunner()
+        );
+
+        $cmdbus->handle(new LocalCommand);
 
         $this->assertTrue(TRUE);
     }
