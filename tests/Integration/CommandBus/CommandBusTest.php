9b549fab2a2da6c0ee8711c764b282d941ba5aa4
Javier Garrido <ProyectoTAU2@gmail.com>
2022-03-20 23:48:41 +0100

    Add several Middleware tests to Tactician CommmandBus

--- tests/Integration/CommandBus/CommandBusTest.php
+++ tests/Integration/CommandBus/CommandBusTest.php
@@ -2,24 +2,27 @@
 
 namespace ProyectoTAU\Tests\Integration\CommandBus;
 
-use League\Tactician\Middleware;
+use Exception;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 use ProyectoTAU\CommandBus\CommandBus as LocalCommandBus;
+use League\Tactician\Middleware;
 use League\Tactician\CommandBus;
+use function Webmozart\Assert\Tests\StaticAnalysis\boolean;
 
 class TestLogger
 {
-    private $testcase;
+    private $logger;
 
-    public function __construct(object $that)
+    public function __construct(&$logger)
     {
-        $this->testcase = $that;
+        $this->logger = &$logger;
     }
 
     public function __call($name, $message)
     {
-        $this->testcase->assertTrue(TRUE);
-        printf("\n[%s] %s\n", $name, $message[0]);
+        $this->logger = true;
+        //printf("\n[%s] %s\n", $name, $message[0]);
     }
 }
 
@@ -39,35 +42,131 @@ class LoggerMiddleware implements Middleware
     }
 }
 
-class CommandsRunner implements Middleware
+class TestCommandsRunner implements Middleware
 {
+    private $runner;
+    private $handler;
+
+    public function __construct(&$runner, &$handler)
+    {
+        $this->runner = &$runner;
+        $this->handler = &$handler;
+    }
+
     public function execute(object $command, callable $next)
     {
+        $this->runner = true;
         $commandName = \get_class($command);
         $commandHandler = $commandName.'Handler';
-        (new $commandHandler)->handle($command);
+        (new $commandHandler($this->handler))->handle($command);
     }
 }
 
-class LocalCommand
+class TestCommand
 {
-
 }
 
-class LocalCommandHandler
+class TestFailedCommand
 {
+}
+
+class TestCommandHandler
+{
+    private $handler;
+
+    public function __construct(&$handler)
+    {
+        $this->handler = &$handler;
+    }
+
     public function handle(object $command)
     {
-        echo PHP_EOL.\get_class($command).' handled'.PHP_EOL;
+        $this->handler = true;
+    }
+}
+
+class TestFailedCommandHandler
+{
+    private $handler;
+
+    public function __construct(&$handler)
+    {
+        $this->handler = &$handler;
+    }
+
+    public function handle(object $command)
+    {
+        $this->handler = true;
+        throw new \Exception('Command Failed');
+    }
+}
+
+class TestTransaction implements Middleware
+{
+    private $entityManager;
+    private $transaction;
+
+    public function __construct(&$transaction, object $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->transaction = &$transaction;
+    }
+
+    public function execute(object $command, callable $next)
+    {
+        $this->entityManager->begin();
+
+        try {
+
+            $next($command);
+
+        } catch (\Exception $e){
+            $this->entityManager->rollback();
+            //throw $e;
+            return;
+        } finally {
+            $this->transaction = true;
+        }
+
+        $this->entityManager->commit();
+    }
+}
+
+class TestEntityManager
+{
+    private $begin;
+    private $commit;
+    private $rollback;
+
+    public function __construct(&$begin, &$commit, &$rollback)
+    {
+        $this->begin = &$begin;
+        $this->commit = &$commit;
+        $this->rollback = &$rollback;
+    }
+
+    public function begin()
+    {
+        $this->begin = true;
+    }
+
+    public function commit()
+    {
+        $this->commit = true;
+    }
+
+    public function rollback()
+    {
+        $this->rollback = true;
     }
 }
 
 class CommandBusTest extends TestCase
 {
-    /**
+     /**
      * @link https://github.com/sebastianbergmann/phpunit-documentation/issues/171#issuecomment-67239415
      */
-    function test_can_make_LocalCommandBus_class(){
+    function skip_test_can_make_LocalCommandBus_class(){
         try {
             $cmdbus = new LocalCommandBus();
         } catch (\InvalidArgumentException $notExpected) {
@@ -91,13 +190,72 @@ class CommandBusTest extends TestCase
     }
 
     function test_can_make_Middleware_class(){
+        $logger = false;
+        $runner = false;
+        $handler = false;
+
         $cmdbus = new CommandBus(
-            new LoggerMiddleware(new TestLogger($this)),
-            new CommandsRunner()
+            new LoggerMiddleware(new TestLogger($logger)),
+            new TestCommandsRunner($runner, $handler)
         );
 
-        $cmdbus->handle(new LocalCommand);
+        $cmdbus->handle(new TestCommand);
 
-        $this->assertTrue(TRUE);
+        $this->assertTrue($logger, 'Logger failed');
+        $this->assertTrue($runner, 'Runner failed');
+        $this->assertTrue($handler, 'Handler failed');
+    }
+
+    function test_can_make_Commited_Transaction(){
+        $logger = false;
+        $transaction = false;
+        $begin = false;
+        $commit = false;
+        $rollback = false;
+
+        $cmdbus = new CommandBus(
+            new LoggerMiddleware(new TestLogger($logger)),
+            new TestTransaction(
+                $transaction,
+                new TestEntityManager($begin, $commit, $rollback)
+            )
+        );
+
+        $cmdbus->handle(new TestCommand);
+
+        $this->assertTrue($logger, 'Logger failed');
+        $this->assertTrue($transaction, 'Transaction failed');
+        $this->assertTrue($begin, 'begin failed');
+        $this->assertTrue($commit, 'commit failed');
+        $this->assertFalse($rollback, 'rollback failed');
+    }
+
+    function test_can_make_Rollbacked_Transaction(){
+        $logger = false;
+        $transaction = false;
+        $begin = false;
+        $commit = false;
+        $rollback = false;
+        $runner = false;
+        $handler = false;
+
+        $cmdbus = new CommandBus(
+            new LoggerMiddleware(new TestLogger($logger)),
+            new TestTransaction(
+                $transaction,
+                new TestEntityManager($begin, $commit, $rollback)
+            ),
+            new TestCommandsRunner($runner, $handler)
+        );
+
+        $cmdbus->handle(new TestFailedCommand);
+
+        $this->assertTrue($logger, 'Logger failed');
+        $this->assertTrue($transaction, 'Transaction failed');
+        $this->assertTrue($begin, 'begin failed');
+        $this->assertFalse($commit, 'commit failed');
+        $this->assertTrue($rollback, 'rollback failed');
+        $this->assertTrue($runner, 'Runner failed');
+        $this->assertTrue($handler, 'Handler failed');
     }
 }
